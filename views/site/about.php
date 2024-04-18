e53e0559d6ea4bb81e475c8914d1ead89df1a9aa
Harut-web-developer <hovsepyanh1994@gmail.com>
2024-04-18 12:56:02 +0400

    change login page

--- views/site/about.php
+++ views/site/about.php
@@ -31,24 +31,15 @@ $this->registerCssFile('@web/css/about.css');
                         <p>By considering a problem, ideas emerge that develop into solutions and improve the efficiency of future business operations. The values we uphold, carry, and believe in serve as the cornerstones of the process from idea to solution. </p>
                     </div>
                 </div>
-                <!--            <h1>Who We Are</h1>-->
-                <!--            <h2 class="leftH21">OUR HISTORY</h2>-->
-                <!--            <p>-->
-                <!--                iNFOEXPERT was established in 1996 and has spent the last 27 years concentrating on the creation, promotion, and support of specialized business automation solutions based on 1C software. The 1C firm's official representative in Armenia with the right to franchise is the iNFOEXPERT company.-->
-                <!--                <br>-->
-                <!--                The company provides systems for resource management, partner relations, personnel management, document circulation, and accounting, automating every aspect of accounting and management procedures.-->
-                <!--                <br>-->
-                <!--                Every partner request is considered, compliance with the regulations in effect in the Republic of Armenia is supplied, and based on the unique characteristics of the industry, a package of customized business process automation solutions is developed.-->
-                <!--                <br>-->
-                <!--                Technology advances with time, as do company models and methods of operation, but what distinguishes iNFOEXPERT as a pioneer are its solutions that aid partners in becoming more competitive and thriving in a rapidly changing environment. Long-lasting and devoted partnership relationships are ensured by the experience gained through time and the capacity to provide swift and flexible solutions.-->
-                <!--            </p>-->
-                <!--            <h2 class="leftH22">OUR VALUES</h2>-->
-                <!--            <p>-->
-                <!--                By considering a problem, ideas emerge that develop into solutions and improve the efficiency of future business operations. The values we uphold, carry, and believe in serve as the cornerstones of the process from idea to solution.-->
-                <!--            </p>-->
             </div>
             <div class="rightContent">
-                <!--            <div class="rightContentTop">-->
+                            <div class="rightContentTop">
+                                <span>OUR VISION</span>
+                                <div class="rightContentMainText">
+                                    <p>Every company endeavor is mostly about people...</p>
+                                    <p>Make sure your potential business partner, client, or consumer feels heard, valued, and never forgotten. A caring partnership is built on these.Whether it's speedy service or resolving a challenging issue, give every partner, customer, and client exactly the experience they desire. This is the trust formula, and with it, it is possible to gain loyalty and build long-term relationships. We combine the best business solutions with a vibrant culture and commitment to our community. "For 27 years, the expansion and success of our company have been powered by a distinctive culture of innovation and problem solving for the most renowned companies in Armenia."</p>
+                                    <p>Founder and CEO: A. Grigoryan</p>
+                                </div>
                 <!--                <h2>OUR VISION</h2>-->
                 <!--                <p>-->
                 <!--                    Every company endeavor is mostly about people...-->
@@ -75,8 +66,55 @@ $this->registerCssFile('@web/css/about.css');
                 <!--                        <img src="/images/aboutImg_3.png" alt="">-->
                 <!--                        <h1>27 years on the market</h1>-->
                 <!--                    </div>-->
-                <!--                </div>-->
-                <!--                <div class="rightContentBottomR">-->
+                                </div>
+                                <div class="rightContentBottom">
+                                    <div class="rightContentInfoShapeLeft">
+                                        <div class="shapeLeft">
+                                            <div class="mainLeftShapeField">
+                                                <img src="/images/aboutImg_1.png" alt="">
+                                                <div class="mainLeftShapeFieldTexts">
+                                                    <span class="shapeFirstTitle">Unrestricted</span>
+                                                    <span class="shapeSecondTitle">Automated solutions</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="shapeLeft">
+                                            <div class="mainLeftShapeField">
+                                                <img src="/images/aboutImg_2.png" alt="">
+                                                <div class="mainLeftShapeOther">
+                                                    <span>Professional Team</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="shapeLeft">
+                                            <div class="mainLeftShapeField">
+                                                <img src="/images/aboutImg_3.png" alt="">
+                                                <div class="mainLeftShapeOther">
+                                                    <div class="spanText">27 years on the market</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="rightContentInfoShapeRight">
+                                        <div class="shapeRight">
+                                            <div class="shapeRightMainField">
+                                                <img src="/images/aboutImg_4.png" alt="">
+                                                <div class="shapeRightMainFieldText">
+                                                    <span class="rightShapeTextBig">Long-term</span>
+                                                    <span class="rightShapeTextSmall">collaboration</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="shapeRight">
+                                            <div class="shapeRightMainField">
+                                                <img src="/images/aboutImg_5.png" alt="">
+                                                <div class="shapeRightBottomText">
+                                                    <span class="rightShapeBottomTextBig">Individual approach</span>
+                                                    <span class="rightShapeBottomTextSmall">to each partner</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                 <!--                    <div class="div4">-->
                 <!--                        <img src="/images/aboutImg_4.png" alt="">-->
                 <!--                        <h1>Long-term</h1>-->
@@ -88,7 +126,7 @@ $this->registerCssFile('@web/css/about.css');
                 <!--                        <h3>to each partner</h3>-->
                 <!--                    </div>-->
                 <!--                </div>-->
-                <!--            </div>-->
+                            </div>
             </div>
         </div>
     </div>
--- views/site/login.php
+++ views/site/login.php
@@ -7,43 +7,39 @@ $this->params['breadcrumbs'][] = $this->title;
 $this->registerCssFile('@web/css/login.css');
 
 ?>
-<section class="mainPoints">
-    <div class="container">
-        <div class="globus">
-            <span class="globusPoints">
-                <img src="/images/unsplash_6ie6OjshvWg.png" class="point1"></img>
-                <p class="point2"></p>
-                <img src="/images/unsplash_6ie6OjshvWg.png" class="point3"></img>
-                <p class="point4"></p>
-            </span>
-            <div class="globusSmooll">
-                <p class="text1">Up Your<span> Skills </span>To <span>Advance</span> Your <span>Career</span> Path</p>
-                <p class="text2">Just a couple of clicks and we start</p>
+<div class="login">
+    <div class="loginField">
+        <div class="textCircle">
+            <div class="mainTextCircle">
+                <div class="circleTextTop">UP YOUR <span>SKILLS</span> TO <span>ADVANCE</span> YOUR <span>CAREER</span> PATH</div>
+                <div class="circleTextBottom">Just a couple of clicks and we start</div>
             </div>
         </div>
         <form action="" class="signin">
-            <h1>WELCOME BACK</h1>
+            <span>WELCOME BACK</span>
             <div class="signinMenu">
                 <input type="email" placeholder="Email Address" name="email">
-                <input type="password" placeholder="Password" name="password">
-                <div class="checkRem">
-                    <label class="container_checked">
-                        <input type="checkbox" checked="checked">
-                        <span class="checkmark"></span>
-                        <p>remember</p>
-                    </label>
-                    <span class="checkbox">
-                    <a href="">Lost your password?</a>
-                </span>
+                <div class="passEye">
+                    <input type="password" placeholder="Password" name="password">
+                    <img src="/images/signEye.png" alt="">
                 </div>
-
+                <div class="rememberField">
+                    <div class="mainCheck">
+                        <input id="checkboxSignIn" type="checkbox" >
+                        <label for="checkboxSignIn">remember</label>
+                    </div>
+                    <a href="">Lost your password?</a>
+                </div>
             </div>
-            <div class="container_signin">
+            <button type="submit" class="container_signin">
                 <img src="/images/Vector.png" alt="">
-                <div class="centered">SIGN IN</div>
+                <span>SIGN IN</span>
+            </button>
+            <div class="signUp">
+                <span>Not a member ?</span>
+                <a href="">Sign Up now</a>
             </div>
-            <p class="signUp">Not a member ? <a href="">Sign Up now</a></p>
         </form>
     </div>
-</section>
+</div>
 
