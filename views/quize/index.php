a226319b3c2b815abf9f7231496ad744bce81649
MariamManukyan <mariammanukyan27012002@gmail.com>
2024-06-06 18:45:42 +0400

    add quize

---
+++ views/quize/index.php
@@ -0,0 +1,25 @@
+<?php
+/** @var yii\web\View $this */
+$this->registerCssFile('@web/css/quize.css');
+?>
+
+<div class="quize">
+    <div class="bodyQuize">
+        <div class="quizeContainer">
+            <div class="quizePlayAndTest">ԽԱՂԱ ԵՎ ՍՏՈՒԳԻՐ ԳԻՏԵԼԻՔՆԵՐԴ</div>
+            <div class="lessonContainer">
+                <div class="lessonAdministration">1С: ԱՌԵՎՏՐԻ ԿԱՌԱՎԱՐՈւՄ</div>
+                <div class="lessonAccounting">1С: ՀԱՇՎԱՊԱՀՈՒԹՅՈՒՆ 8.3</div>
+                <div class="lessonAccountingForBeginners">1C: ՀԱՇՎԱՊԱՀՈՒԹՅՈՒՆ ՍԿՍՆԱԿՆԵՐԻ ՀԱՄԱՐ</div>
+                <div class="lessonPersonnelManagment">1С: ԱՇԽԱՏԱՎԱՐՁ ԵՒ ԿԱԴՐԵՐԻ ԿԱՌԱՎԱՐՈՒՄ</div>
+                <div class="lessonPersonnelManagment">1С: ԱՇԽԱՏԱՎԱՐՁ ԵՒ ԿԱԴՐԵՐԻ ԿԱՌԱՎԱՐՈՒՄ</div>
+                <div class="lessonPersonnelManagment">1С: ԱՇԽԱՏԱՎԱՐՁ ԵՒ ԿԱԴՐԵՐԻ ԿԱՌԱՎԱՐՈՒՄ</div>
+                <div class="lessonPersonnelManagment">1С: ԱՇԽԱՏԱՎԱՐՁ ԵՒ ԿԱԴՐԵՐԻ ԿԱՌԱՎԱՐՈՒՄ</div>
+            </div>
+            <button type="button" class="btnQuize">
+                <img src="/images/contactus.png" alt="">
+                <span>ՍԿՍԵԼ</span>
+            </button>
+        </div>
+    </div>
+</div>
