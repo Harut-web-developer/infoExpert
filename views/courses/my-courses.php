091ff3861f4eba59fa763af8210b93fe9d4c8eaa
Mariam Manukyan <mariammanukyan27012002@gmail.com>
2024-06-13 23:04:54 -0800

    add my courses carousel js

--- views/courses/my-courses.php
+++ views/courses/my-courses.php
@@ -251,3 +251,59 @@ if ($language == 'en') {
         </div>
     </div>
 </div>
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        function slider(carousel, wrapper, firstCard) {
+            const firstCardWidth = firstCard.offsetWidth;
+            let isDragging = false,
+                startX,
+                startScrollLeft,
+                timeoutId;
+
+            const dragStart = (e) => {
+                isDragging = true;
+                carousel.classList.add("dragging");
+                startX = e.touches ? e.touches[0].pageX : e.pageX;
+                startScrollLeft = carousel.scrollLeft;
+            };
+
+            const dragging = (e) => {
+                if (!isDragging) return;
+                const newScrollLeft = startScrollLeft - ((e.touches ? e.touches[0].pageX : e.pageX) - startX);
+                if (newScrollLeft <= 0 || newScrollLeft >= carousel.scrollWidth - carousel.offsetWidth) {
+                    isDragging = false;
+                    return;
+                }
+                carousel.scrollLeft = newScrollLeft;
+            };
+
+            const dragStop = () => {
+                isDragging = false;
+                carousel.classList.remove("dragging");
+            };
+
+            const autoPlay = () => {
+                if (window.innerWidth < 800) return;
+                const totalCardWidth = carousel.scrollWidth;
+                const maxScrollLeft = totalCardWidth - carousel.offsetWidth;
+                if (carousel.scrollLeft >= maxScrollLeft) return;
+            };
+
+            carousel.addEventListener("touchstart", dragStart);
+            carousel.addEventListener("mousedown", dragStart);
+            carousel.addEventListener("touchmove", dragging);
+            carousel.addEventListener("mousemove", dragging);
+            document.addEventListener("touchend", dragStop);
+            document.addEventListener("mouseup", dragStop);
+            wrapper.addEventListener("mouseenter", () =>
+                clearTimeout(timeoutId));
+            wrapper.addEventListener("mouseleave", autoPlay);
+        }
+        const wrapper = document.querySelector(".wrapperMyCourses");
+        const carousel = document.querySelector(".carouselMyCourses");
+        if (carousel != null){
+            const firstCard = carousel.querySelector(".myCoursesBlocksField");
+            slider(carousel, wrapper, firstCard);
+        }
+    });
+</script>
