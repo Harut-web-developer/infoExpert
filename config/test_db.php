b6dc5136a058571cc5a44d5b11012622ca8e3d7d
crmarm <crmarm313@gmail.com>
2024-07-24 06:12:13 +0000

    new

--- config/test_db.php
+++ config/test_db.php
@@ -1,6 +1,13 @@
 <?php
-$db = require __DIR__ . '/db.php';
-// test database! Important not to run tests on production or development databases
-$db['dsn'] = 'mysql:host=localhost;dbname=yii2basic_test';
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=infoexpert',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
 
-return $db;
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
